7735d10228cc9218bf08db9208301794c3efc168
Trisnawan <trisna5biru@gmail.com>
2024-06-02 09:55:02 +0700

    Login bal 2

--- app/Controllers/Login.php
+++ app/Controllers/Login.php
@@ -21,14 +21,17 @@ class Login extends ResourceController
             ], 200);
         }
         $user = $this->model->where('email', $post['email'])->first();
-        if(password_verify($post['password'], $user['password'])){
-            unset($user['password']);
-            return $this->respond([
-                "status" => true,
-                "message" => "Success",
-                "data" => $user
-            ], 200);
+        if($user){
+            if(password_verify($post['password'], $user['password'])){
+                unset($user['password']);
+                return $this->respond([
+                    "status" => true,
+                    "message" => "Success",
+                    "data" => $user
+                ], 200);
+            }
         }
+        
         return $this->respond([
             "status" => false,
             "message" => "Email atau kata sandi tidak cocok",
